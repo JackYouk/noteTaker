24e69422fa4d7d114585461cd9cc1607ac8e71c4
JackYouk <jackyoukstetter1@gmail.com>
2022-07-31 18:37:20 -0700

    fixed post route and added comments to the routes

--- routes/apiRoutes/notesRoutes/index.js
+++ routes/apiRoutes/notesRoutes/index.js
@@ -17,18 +17,11 @@ router.get('/', (req, res) => {
 
 // post/save note
 router.post('/', (req, res) => {
+    // saves posted note object as newNote
     const newNote = req.body;
-    fs.writeFile('../../../db/db.json', JSON.stringify(newNote), (err) => {
-        console.error(err);
-        res.json(err);
-    });
-    res.json(newNote);
-});
 
-// delete note
-router.delete('/:id', (req, res) => {
+    // gets the current notes array from the db
     let notes;
-    const id = req.params.id;
     fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
         if(err){
             console.error(err);
@@ -36,17 +29,45 @@ router.delete('/:id', (req, res) => {
         }
         notes = JSON.parse(data);
     });
+
+    // adds new note to the db data
+    notes.push(newNote);
+
+    // rewrites the database with the updated data
+    fs.writeFile('../../../db/db.json', JSON.stringify(notes), (err) => {
+        console.error(err);
+        res.json(err);
+    });
+
+    res.json(newNote);
+});
+
+// delete note
+router.delete('/:id', (req, res) => {
+    let notes;
+    const id = req.params.id;
+    // gets the current data from db
+    fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+            res.json(err);
+        }
+        notes = JSON.parse(data);
+    });
+    // loops thru data to find matching id, deletes note object from the array
     for(let i = 0; i < notes.length; i++){
         if(notes[i].id === parseInt(id)){
             notes.splice(notes[i]);
         }
     }
 
+    // rewrites the database file with the updated data
     fs.writeFile('../../../db/db.json', JSON.stringify(notes), (err) => {
         console.error(err);
         res.json(err);
     });
-    res.json()
+
+    res.json(notes);
 });
 
 
