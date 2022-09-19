e6811b17a21ace2fc05fedd735a08c031d47fbfe
JackYouk <jackyoukstetter1@gmail.com>
2022-09-19 16:42:22 -0700

    deploy bug fixes

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ router.use('/api', apiRoutes);
 // HTML pages ==================
 // landing page route
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 // notes page route
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 })
 
 module.exports = router;
